hanzo-ds/connect

Release 0 8 16 (#485)
* Check for optional libraries in client methods

* Log unexpected http next chunk unexpected

* Log unexpected http next chunk unexpected

* Updates for 0.8.16 release

clickhouse_connect/common.py
@@ -41,8 +41,9 @@ def build_client_name(client_name: str):
             os_user = f'; os_user:{getpass.getuser()}'
         except Exception:  # pylint: disable=broad-except
             pass
-    return (f'{client_name}{product_name}clickhouse-connect/{version()}' +
+    full_name = (f'{client_name}{product_name}clickhouse-connect/{version()}' +
             f' (lv:py/{py_version}; mode:sync; os:{sys.platform}{os_user})')
+    return full_name.encode('ascii', 'ignore').decode()
 
 
 def get_setting(name: str):

timeplus-io/timeplus-connect

timeplus_connect/common.py
@@ -41,8 +41,9 @@ def build_client_name(client_name: str):
             os_user = f'; os_user:{getpass.getuser()}'
         except Exception:  # pylint: disable=broad-except
             pass
-    return (f'{client_name}{product_name}timeplus-connect/{version()}' +
+    full_name = (f'{client_name}{product_name}timeplus-connect/{version()}' +
             f' (lv:py/{py_version}; mode:sync; os:{sys.platform}{os_user})')
+    return full_name.encode('ascii', 'ignore').decode()
 
 
 def get_setting(name: str):